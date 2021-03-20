4e685a29ce99d63d74562dcea683307b0499e1f0
Vedant <karlevedant7@gmail.com>
2021-03-20 12:04:24 +0530

    Text Field Added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Notes from "./pages/Notes";
 import Create from "./pages/Create";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import { purple } from "@material-ui/core/colors";
 
 const theme = createMuiTheme({
   palette: {
     primary: {
       main: "#fefefe",
     },
+    secondary: purple,
+  },
+  typography: {
+    fontFamily: "Quicksand",
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
   },
 });
 
